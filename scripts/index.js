afd5f2e77fb3ba0b15d23065355276ac7028173b
clericlvl2 <budarnykh.a@yandex.ru>
2022-10-11 19:47:23 +0700

    feat: add popup exit on overlay click and "esc" key + minor event handlers refactoring

--- scripts/index.js
+++ scripts/index.js
@@ -6,88 +6,115 @@ const userAddBtn = document.querySelector('.profile__btn-add');
 const gallery = document.querySelector('.gallery__list');
 const cardTemplate = document.querySelector('#js-card-template').content;
 
-// popup edit
+// popup edit profile
 const popupEditProfile = document.querySelector('.popup_action_edit');
 const formEditProfile = document.querySelector('.popup__form_data_profile');
-const userNameInput =  document.querySelector('.popup__form-text_type_name');
+const userNameInput = document.querySelector('.popup__form-text_type_name');
 const userProfessionInput = document.querySelector('.popup__form-text_type_desc');
 
-// popup add
+// popup add card
 const popupAddCard = document.querySelector('.popup_action_add');
 const formAddCard = document.querySelector('.popup__form_data_card');
-const cardTitleInput =  document.querySelector('.popup__form-text_type_card-title');
+const cardTitleInput = document.querySelector('.popup__form-text_type_card-title');
 const cardImageInput = document.querySelector('.popup__form-text_type_card-image');
 
-// popup zoom
-const popupZoom = document.querySelector('.popup_action_zoom');
+// popup zoom image
+const popupZoomImage = document.querySelector('.popup_action_zoom');
 const zoomedImage = document.querySelector('.popup__image');
 const zoomedText = document.querySelector('.popup__image-subtitle');
 
-// btn array
-const popupExitButtons = document.querySelectorAll('.popup__btn-exit');
-const exitButtonArray = Array.from(popupExitButtons);
+const popupArray = [popupEditProfile, popupAddCard, popupZoomImage];
 
 // app logic functions
 function openPopup(element) {
   element.classList.add('popup_opened');
 }
+
 function closePopup(element) {
   element.classList.remove('popup_opened');
 }
+
 function submitUserData(event) {
   event.preventDefault();
   userName.textContent = userNameInput.value;
   userDesc.textContent = userProfessionInput.value;
   closePopup(popupEditProfile);
 }
-function submitCardData(event) {
-  event.preventDefault();
-  const data = {};
-  data.name = cardTitleInput.value;
-  data.link = cardImageInput.value;
-  addCard(data);
-  closePopup(popupAddCard);
-  formAddCard.reset();
-}
-function addCard(data) {
-  const newCard = createCard(data);
-  gallery.prepend(newCard);
-}
+
 function createCard(data) {
   const card = cardTemplate.querySelector('.card').cloneNode(true);
   const cardTitle = card.querySelector('.card__title');
   const cardImage = card.querySelector('.card__image');
-  const cardLikeBtn = card.querySelector('.card__btn-like');
-  const cardDeleteBtn = card.querySelector('.card__btn-delete');
   cardTitle.textContent = data.name;
   cardImage.src = data.link;
   cardImage.alt = 'Фото. ' + data.name;
-  cardLikeBtn.addEventListener('click', evt => {
-    evt.target.classList.toggle('card__btn-like_active');
-  });
-  cardDeleteBtn.addEventListener('click', evt => {
-    evt.target.closest('.card').remove();
-  });
-  cardImage.addEventListener('click', () => {
-    zoomedImage.src = cardImage.src;
-    zoomedImage.alt = cardImage.alt;
-    zoomedText.textContent = cardTitle.textContent;
-    openPopup(popupZoom);
-  });
   return card;
 }
 
-// add listeners to buttons
-exitButtonArray.forEach(item => {
-  item.addEventListener('click', evt => {
-    closePopup(evt.target.closest('.popup'));
-  });
+function addCard(data) {
+  const newCard = createCard(data);
+  gallery.prepend(newCard);
+}
+
+function submitCardData(evt) {
+  evt.preventDefault();
+  const data = {
+    name: cardTitleInput.value,
+    link: cardImageInput.value,
+  };
+  addCard(data);
+  closePopup(popupAddCard);
+  formAddCard.reset();
+}
+
+function exitPopupClickHandler(evt) {
+  const isOverlay = evt.target === evt.currentTarget;
+  const isExitBtn = evt.target.classList.contains('popup__btn-exit');
+  if (isOverlay) {
+    closePopup(evt.currentTarget);
+  }
+  if (isExitBtn) {
+    closePopup(evt.currentTarget);
+  }
+}
+
+function exitPopupKeyHandler(evt) {
+  const activePopup = document.querySelector('.popup_opened');
+  if (evt.key === 'Escape' && activePopup) {
+    closePopup(activePopup);
+  }
+}
+
+document.addEventListener('keydown', exitPopupKeyHandler);
+popupArray.forEach(popup => {
+  popup.addEventListener('click', exitPopupClickHandler);
 })
-userAddBtn.addEventListener('click', () => openPopup(popupAddCard));
+
+gallery.addEventListener('click', evt => {
+  const isLikeBtn = evt.target.classList.contains('card__btn-like');
+  const isDeleteBtn = evt.target.classList.contains('card__btn-delete');
+  const isImage = evt.target.classList.contains('card__image');
+  if (isLikeBtn) {
+    evt.target.classList.toggle('card__btn-like_active');
+  }
+  if (isDeleteBtn) {
+    evt.target.closest('.card').remove();
+  }
+  if (isImage) {
+    const cardImage = evt.target;
+    const cardTitle = evt.target.closest('.card').querySelector('.card__title');
+    zoomedImage.src = cardImage.src;
+    zoomedImage.alt = cardImage.alt;
+    zoomedText.textContent = cardTitle.textContent;
+    openPopup(popupZoomImage);
+  }
+})
+
 userEditBtn.addEventListener('click', () => {
   userNameInput.value = userName.textContent;
   userProfessionInput.value = userDesc.textContent;
   openPopup(popupEditProfile);
 });
-formAddCard.addEventListener('submit', submitCardData);
+userAddBtn.addEventListener('click', () => openPopup(popupAddCard));
 formEditProfile.addEventListener('submit', submitUserData);
+formAddCard.addEventListener('submit', submitCardData);
